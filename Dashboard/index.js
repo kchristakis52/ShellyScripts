14e052c8515f85edc60e71238492de4f4b873c1d
Konstantinos Christakis <kxristakis@outlook.com>
2024-02-07 22:26:40 +0200

    Add initial files for the dashboard application

---
+++ Dashboard/index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const mqtt = require('mqtt');
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.json());
+const port = 3000;
+
+// Create a connection to the SQLite database
+const db = new sqlite3.Database('gateways.db');
+
+// Define the endpoint for the landing page
+app.get('/', (req, res) => {
+    // Fetch data from the database
+    db.all('SELECT * FROM gateways', (err, rows) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            // Render the landing page with the fetched data
+            res.render('landing_page', { data: rows });
+        }
+    });
+});
+
+app.get('/devices', (req, res) => {
+    const gatewayId = req.query.gateway
+    // Fetch data from the database
+    db.all('SELECT * FROM devices WHERE gateway_uuid = ?', [gatewayId], (err, rows) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            // Render the landing page with the fetched data
+            res.render('devices', { data: rows });
+        }
+    });
+});
+
+app.post('/toggle_switch', (req, res) => {
+
+    const topic = req.body.gateway_uuid
+    const message = req.body.device_id
+    console.log(topic)
+    console.log(message)
+    // Send a message with MQTT
+    const client = mqtt.connect('mqtt://192.168.1.2');
+    client.on('connect', () => {
+        client.publish(topic, message);
+        client.end();
+    });
+
+    res.send('Message sent with MQTT');
+});
+
+
+
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
