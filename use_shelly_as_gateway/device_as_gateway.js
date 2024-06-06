d9e0aee98209cf5e0777dca0ea6defa3d400fa93
Konstantinos Christakis <kxristakis@outlook.com>
2024-06-07 00:54:03 +0300

    Refactor device_as_gateway.js to include response for TRV commands

--- use_shelly_as_gateway/device_as_gateway.js
+++ use_shelly_as_gateway/device_as_gateway.js
@@ -1,6 +1,7 @@
 function processMessage(topic, message, userdata) {
     messageObject = JSON.parse(message)
     let url = ""
+    let res_url = ""
     if (messageObject.device_type == "trv") {
         if (messageObject.action == "target_temperature") {
             url = "http://" + messageObject.id + "/settings/thermostats/0?target_t=" + messageObject.value
@@ -8,6 +9,7 @@ function processMessage(topic, message, userdata) {
         else if (messageObject.action == "valve_position") {
             url = "http://" + messageObject.id + "/thermostats/0?pos=" + messageObject.value
         }
+        res_url = "http://" + messageObject.id + "/status"
     }
     else if (messageObject.device_type == "plug") {
         url = "http://" + messageObject.id + "/relay/0?turn=" + messageObject.value
@@ -21,8 +23,29 @@ function processMessage(topic, message, userdata) {
                 print(error_message)
                 return;
             }
-            print(data)
-
+            if (res_url == "") {
+                MQTT.publish("response_topic", JSON.stringify(data.body))
+                return;
+            }
+            else {
+                Shelly.call(
+                    "http.get",
+                    { "url": res_url },
+                    function (data, error_code, error_message) {
+                        if (error_code != 0) {
+                            print(error_message)
+                            return;
+                        }
+                        body = JSON.parse(data.body)
+                        print(body)
+                        responseJSON = {
+                            thermostats: body.thermostats,
+                            battery: body.bat
+                        }
+                        MQTT.publish("response_topic", JSON.stringify(responseJSON))
+                    }
+                );
+            }
         }
     );
 }
