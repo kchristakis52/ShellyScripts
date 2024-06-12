9e049205c8b9cc6bb9f899ef698837158adeaca1
Konstantinos Christakis <kxristakis@outlook.com>
2024-06-12 20:13:43 +0300

    Update endpoint.js to display TRV data in a user-friendly format

--- use_shelly_as_gateway/endpoint.js
+++ use_shelly_as_gateway/endpoint.js
@@ -1,39 +1,30 @@
 function GetPwr(request, response) {
-    let userdata = ["request", "response"]
     Shelly.call("KVS.GetMany", { match: "trv*" }, function (result, error_code, error_message) {
 
         if (error_code != 0) {
             // process error
         } else {
             items = result.items
-            let htmlbody = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>htmx Example</title><script src="https://unpkg.com/htmx.org@1.8.1/dist/htmx.min.js"></script></head><body>'
-            let dropdown = '<label for="dropdown">Choose an option:</label><select id="dropdown" hx-trigger="change from:#dropdown"> <option value="">Select...</option>'
-            let divs = '<div id="content">'
+            let a = JSON.stringify(items)
+
+            let htmlbody = '<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8" /> <meta name="viewport" content="width=device-width, initial-scale=1.0" /> <title>TRV Data</title> <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet" /> </head> <body class="container my-4"> <div class="container mx-auto g-3"> <h1>TRV Data</h1> <div class="mb-3"> <label for="dropdown" class="form-label">Choose an option:</label> <select class="form-select" id="dropdown">'
+            htmlbody += '<option value="">Select...</option>'
+
             for (let key in items) {
                 if (items.hasOwnProperty(key)) {
                     console.log(key); // item1, item2
                     console.log(items[key].etag); // 0DWty8HwCB, 0DMHqWL0P0
                     console.log(items[key].value); // item1 value, item2 value
                     trvData = JSON.parse(items[key].value)
-                    dropdown += '<option value="' + key + '">' + key + '</option>'
-                    alldivs = "<div>" + trvData.valve_position + "</div> <div>" + trvData.battery + "</div>"
-                    divs += '<div id="' + key + '" class="content-item">' + alldivs + '</div>'
-
-
+                    htmlbody += '<option value="' + key + '">' + key + '</option>'
                 }
             }
-            dropdown += '</select>'
-            divs += '</div>'
-            htmlbody += dropdown + divs
-            htmlbody += "<style>.content-item {display: none;}</style><script>document.getElementById('dropdown').addEventListener('change', function() {var selectedValue = this.value;var contentItems = document.querySelectorAll('.content-item');contentItems.forEach(function(item) {item.style.display = 'none';});if (selectedValue) {var selectedContent = document.getElementById(selectedValue);if (selectedContent) {selectedContent.style.display = 'block';}}});</script></body></html>"
-            response.body = htmlbody
+
+            htmlbody += '</select> </div> <div class="row justify-content-center align-items-center"> <div class="col-md-4"> <div class="card" style="width: 18rem"> <div class="card-body"> <h5 class="card-title">Battery</h5> <div id="battery" class="card-text"></div> </div> </div> </div> <div class="col-md-4"> <div class="card" style="width: 18rem"> <div class="card-body"> <h5 class="card-title">Target Temperature</h5> <div id="target_temperature" class="card-text"></div> </div> </div> </div> <div class="col-md-4"> <div class="card" style="width: 18rem"> <div class="card-body"> <h5 class="card-title">Room Temperature</h5> <div id="ext_temperature" class="card-text"></div> </div> </div> </div> <div class="col-md-4"> <div class="card" style="width: 18rem"> <div class="card-body"> <h5 class="card-title">Valve</h5> <div id="valve" class="card-text"></div> </div> </div> </div> </div> </div> <script> const parsedObject = ' + a + '; for (const key in parsedObject) { if (parsedObject[key].hasOwnProperty("value")) { parsedObject[key].value = JSON.parse(parsedObject[key].value); } } target_temperature = document.getElementById("target_temperature"); external_temperature = document.getElementById("ext_temperature"); battery = document.getElementById("battery"); valve = document.getElementById("valve"); document .getElementById("dropdown") .addEventListener("change", function () { let selectedKey = this.value; battery.textContent = parsedObject[selectedKey].value.battery; target_temperature.textContent = parsedObject[selectedKey].value.target_t; valve.textContent = parsedObject[selectedKey].value.valve_position; external_temperature.textContent = parsedObject[selectedKey].value.temperature; }); </script> <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script> </body> </html> '//htmlbody+='</select> </div> <div class="row justify-content-center align-items-center"> <div class="col-md-4"> <div class="card" style="width: 18rem"> <div class="card-body"> <h5 class="card-title">Battery</h5> <div id="battery" class="card-text"></div> </div> </div> </div> <div class="col-md-4"> <div class="card" style="width: 18rem"> <div class="card-body"> <h5 class="card-title">Temperature</h5> <div id="temperature" class="card-text"></div> </div> </div> </div> </div> </div> <script> const parsedObject = '+a+'; for (const key in parsedObject) { if (parsedObject[key].hasOwnProperty("value")) { parsedObject[key].value = JSON.parse(parsedObject[key].value); } } target_temperature = document.getElementById("temperature"); battery = document.getElementById("battery"); document .getElementById("dropdown") .addEventListener("change", function () { let selectedKey = this.value; battery.textContent = parsedObject[selectedKey].value.battery; target_temperature.textContent = parsedObject[selectedKey].value.target_t; }); </script> <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script> </body> </html>'
+            response.body = htmlbody;
             response.code = 200;
             response.send();
         }
     });
-
 }
-
 HTTPServer.registerEndpoint('pwr', GetPwr)
-// visit to http://<ip of device>/script/<id of script>/pwr
-// More about https://shelly-api-docs.shelly.cloud/gen2/0.14/Scripts/ShellyScriptLanguageFeatures#httpserverregisterendpoint
