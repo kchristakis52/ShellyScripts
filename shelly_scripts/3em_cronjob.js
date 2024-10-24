6f0b42bd4bac60ddc62a662b30aebf5987874c03
Konstantinos Christakis <kxristakis@outlook.com>
2024-10-24 19:21:57 +0300

    Add cronjob scripts for publishing energy data to MQTT

---
+++ shelly_scripts/3em_cronjob.js
@@ -0,0 +1,80 @@
+let CONFIG = {
+    KVS_KEY: "Script-Schedule-" + JSON.stringify(Shelly.getCurrentScriptId()),
+    SCHEDULE_TIMESPEC: "0 0 * * * *",
+    SCHEDULE_ID: -1,
+};
+
+function registerIfNotRegistered() {
+    print("Reading from ", CONFIG.KVS_KEY);
+    Shelly.call(
+        "KVS.Get",
+        {
+            key: CONFIG.KVS_KEY,
+        },
+        function (result, error_code, error_message) {
+            print("Read from KVS", JSON.stringify(error_code));
+            //we are not registered yet
+            if (error_code !== 0) {
+                installSchedule();
+                return;
+            }
+            CONFIG.SCHEDULE_ID = result.value;
+            //check if the schedule was deleted and reinstall
+            Shelly.call("Schedule.List", {}, function (result) {
+                let i = 0;
+                for (i = 0; i < result.jobs.length; i++) {
+                    if (result.jobs[i].id === CONFIG.SCHEDULE_ID) return;
+                }
+                installSchedule();
+            });
+        }
+    );
+}
+
+function saveScheduleIDInKVS(scheduleId) {
+    Shelly.call("KVS.Set", {
+        key: CONFIG.KVS_KEY,
+        value: scheduleId,
+    });
+}
+
+function installSchedule() {
+    Shelly.call(
+        "Schedule.Create",
+        {
+            enable: true,
+            timespec: CONFIG.SCHEDULE_TIMESPEC,
+            calls: [
+                {
+                    method: "script.eval",
+                    params: {
+                        id: Shelly.getCurrentScriptId(),
+                        code: "scheduledTask()",
+                    },
+                },
+            ],
+        },
+        function (result) {
+            //save a record that we are registered
+            saveScheduleIDInKVS(result.id);
+        }
+    );
+}
+
+registerIfNotRegistered();
+
+let id = Shelly.getDeviceInfo().id;
+
+function scheduledTask() {
+    let EMData = Shelly.getComponentStatus("EMData", 0);
+    let mqtt_mess = {
+        Timestamp: new Date().toISOString(),
+        Measurements: [{
+            Value: {
+                aenergy: EMData.total_act,
+                ret_aenergy: EMData.total_act_ret
+            }
+        }]
+    }
+    MQTT.publish("inherit/3EM/" + id + "/data", JSON.stringify(mqtt_mess), 0, true);
+}
