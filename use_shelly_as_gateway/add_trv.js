ac49d80271a1801b3735b51942ae5c12f5a89925
Konstantinos Christakis <kxristakis@outlook.com>
2024-11-13 15:46:45 +0200

    Add script to store TRV data in KVS for multiple rooms

---
+++ use_shelly_as_gateway/add_trv.js
@@ -0,0 +1,258 @@
+let trv_data = {
+    "Timestamp": "2024-09-05T07:00:06.696Z",
+    "Measurements": [
+        {
+            "Value": {
+                "Hostname": "shellytrv-8cf681e9a780",
+                "ValvePosition": 100,
+                "TargetTemperature": 5,
+                "Temperature": 27.6,
+                "Battery": 53
+            }
+        }
+    ]
+};
+
+Shelly.call("KVS.set", {
+    key: "trv/First Floor/Corridor",
+    value: JSON.stringify(trv_data),
+});
+
+let trv_data = {
+    "Timestamp": "2024-09-05T07:00:06.696Z",
+    "Measurements": [
+        {
+            "Value": {
+                "Hostname": "shellytrv-842e14fe1fc2",
+                "ValvePosition": 100,
+                "TargetTemperature": 5,
+                "Temperature": 27.6,
+                "Battery": 53
+            }
+        }
+    ]
+};
+
+Shelly.call("KVS.set", {
+    key: "trv/Classroom 1/1",
+    value: JSON.stringify(trv_data),
+});
+
+let trv_data = {
+    "Timestamp": "2024-09-05T07:00:06.696Z",
+    "Measurements": [
+        {
+            "Value": {
+                "Hostname": "shellytrv-842e14ffcb48",
+                "ValvePosition": 100,
+                "TargetTemperature": 5,
+                "Temperature": 27.6,
+                "Battery": 53
+            }
+        }
+    ]
+};
+
+Shelly.call("KVS.set", {
+    key: "trv/Corridor/4",
+    value: JSON.stringify(trv_data),
+});
+let trv_data = {
+    "Timestamp": "2024-09-05T07:00:06.696Z",
+    "Measurements": [
+        {
+            "Value": {
+                "Hostname": "shellytrv-8cf681a51bae",
+                "ValvePosition": 100,
+                "TargetTemperature": 5,
+                "Temperature": 27.6,
+                "Battery": 53
+            }
+        }
+    ]
+};
+
+Shelly.call("KVS.set", {
+    key: "trv/First Floor/Office room 1",
+    value: JSON.stringify(trv_data),
+});
+
+let trv_data = {
+    "Timestamp": "2024-09-05T07:00:06.696Z",
+    "Measurements": [
+        {
+            "Value": {
+                "Hostname": "shellytrv-8cf681a52e2e",
+                "ValvePosition": 100,
+                "TargetTemperature": 5,
+                "Temperature": 27.6,
+                "Battery": 53
+            }
+        }
+    ]
+};
+
+Shelly.call("KVS.set", {
+    key: "trv/Kitchen/3",
+    value: JSON.stringify(trv_data),
+});
+
+let trv_data = {
+    "Timestamp": "2024-09-05T07:00:06.696Z",
+    "Measurements": [
+        {
+            "Value": {
+                "Hostname": "shellytrv-8cf681b70b5a",
+                "ValvePosition": 100,
+                "TargetTemperature": 5,
+                "Temperature": 27.6,
+                "Battery": 53
+            }
+        }
+    ]
+};
+
+Shelly.call("KVS.set", {
+    key: "trv/Corridor/7",
+    value: JSON.stringify(trv_data),
+});
+
+let trv_data = {
+    "Timestamp": "2024-09-05T07:00:06.696Z",
+    "Measurements": [
+        {
+            "Value": {
+                "Hostname": "shellytrv-8cf681b70e6c",
+                "ValvePosition": 100,
+                "TargetTemperature": 5,
+                "Temperature": 27.6,
+                "Battery": 53
+            }
+        }
+    ]
+};
+
+Shelly.call("KVS.set", {
+    key: "trv/Bathroom/1",
+    value: JSON.stringify(trv_data),
+});
+
+let trv_data = {
+    "Timestamp": "2024-09-05T07:00:06.696Z",
+    "Measurements": [
+        {
+            "Value": {
+                "Hostname": "shellytrv-8cf681b9c924",
+                "ValvePosition": 100,
+                "TargetTemperature": 5,
+                "Temperature": 27.6,
+                "Battery": 53
+            }
+        }
+    ]
+};
+
+Shelly.call("KVS.set", {
+    key: "trv/First Floor/Social room",
+    value: JSON.stringify(trv_data),
+});
+
+let trv_data = {
+    "Timestamp": "2024-09-05T07:00:06.696Z",
+    "Measurements": [
+        {
+            "Value": {
+                "Hostname": "shellytrv-8cf681be1032",
+                "ValvePosition": 100,
+                "TargetTemperature": 5,
+                "Temperature": 27.6,
+                "Battery": 53
+            }
+        }
+    ]
+};
+
+Shelly.call("KVS.set", {
+    key: "trv/Classroom 5/4",
+    value: JSON.stringify(trv_data),
+});
+
+let trv_data = {
+    "Timestamp": "2024-09-05T07:00:06.696Z",
+    "Measurements": [
+        {
+            "Value": {
+                "Hostname": "shellytrv-8cf681cd22c2",
+                "ValvePosition": 100,
+                "TargetTemperature": 5,
+                "Temperature": 27.6,
+                "Battery": 53
+            }
+        }
+    ]
+};
+
+Shelly.call("KVS.set", {
+    key: "trv/Kitchen/1",
+    value: JSON.stringify(trv_data),
+});
+
+let trv_data = {
+    "Timestamp": "2024-09-05T07:00:06.696Z",
+    "Measurements": [
+        {
+            "Value": {
+                "Hostname": "shellytrv-8cf681d9a228",
+                "ValvePosition": 100,
+                "TargetTemperature": 5,
+                "Temperature": 27.6,
+                "Battery": 53
+            }
+        }
+    ]
+};
+
+Shelly.call("KVS.set", {
+    key: "trv/Corridor/6",
+    value: JSON.stringify(trv_data),
+});
+
+let trv_data = {
+    "Timestamp": "2024-09-05T07:00:06.696Z",
+    "Measurements": [
+        {
+            "Value": {
+                "Hostname": "shellytrv-8cf681d9a230",
+                "ValvePosition": 100,
+                "TargetTemperature": 5,
+                "Temperature": 27.6,
+                "Battery": 53
+            }
+        }
+    ]
+};
+
+Shelly.call("KVS.set", {
+    key: "trv/Director's office/1",
+    value: JSON.stringify(trv_data),
+});
+
+let trv_data = {
+    "Timestamp": "2024-09-05T07:00:06.696Z",
+    "Measurements": [
+        {
+            "Value": {
+                "Hostname": "shellytrv-b4e3f9e30ab3",
+                "ValvePosition": 100,
+                "TargetTemperature": 5,
+                "Temperature": 27.6,
+                "Battery": 53
+            }
+        }
+    ]
+};
+
+Shelly.call("KVS.set", {
+    key: "trv/Corridor/5",
+    value: JSON.stringify(trv_data),
+});
